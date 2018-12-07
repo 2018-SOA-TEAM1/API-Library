2347d561697fc8ee1c6f0b7f09ad0df669a8ba3f
sam <s5078345@gmail.com>
2018-12-07 14:53:11 +0800

    Delete old domain and infrastructure folders.

--- app/domain/gameplays/init.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-folders = %w[entities mappers repositories values]
-folders.each do |folder|
-  require_relative "#{folder}/init.rb"
-end
--- app/init.rb
+++ app/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[domain infrastructure application]
+folders = %w[presentation infrastructure application]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
